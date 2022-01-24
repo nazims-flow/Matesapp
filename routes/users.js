25dc30d07504ae3014180983856b06b3aeec50a7
nazims-flow <nazimmazumder68@gmail.com>
2022-01-24 17:05:13 +0530

    users post controller created

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,15 @@ const res = require('express/lib/response');
 const router = express.Router()  
 
 const usersController= require('../controllers/users_controller');
+const usersPostController = require('../controllers/posts_controller')
 
 
 router.get('/', function(req,res){
     res.send('nothing after user')
 })
 router.get('/profile' ,usersController.profile);
+
+router.get('/posts',usersPostController)
 
 
 module.exports =router;
