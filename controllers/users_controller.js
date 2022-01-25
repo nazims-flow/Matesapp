ad957f03123b64d7eff2c021d82b47b4e49e5266
nazims-flow <nazimmazumder68@gmail.com>
2022-01-25 22:30:57 +0530

    added mongo store for the storing of cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,7 +2,8 @@ const User = require('../models/user');
 
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile', {
+   
+    return res.render('profile', {
         title: 'User Profile'
     })
 }
@@ -10,6 +11,9 @@ module.exports.profile = function(req, res){
 
 // render the sign up page
 module.exports.signUp = function(req, res){
+    if(req.isAuthenticated()){
+        res.redirect('/users/profile');
+    }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
@@ -18,6 +22,9 @@ module.exports.signUp = function(req, res){
 
 // render the sign in page
 module.exports.signIn = function(req, res){
+    if(req.isAuthenticated()){
+        res.redirect('/users/profile');
+    }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const cookieParser = require('cookie-parser');
 const app = express();
 const port = 800;
 const expressLayouts = require('express-ejs-layouts');
-const db = require('./config/mongoose');
-
 // used for session cookie
 const session = require('express-session');
+const db = require('./config/mongoose');
+const MongoStore =require('connect-mongo');
+
+
 
 const passport= require('passport');  // requiring both is necessary
 const passportLocal = require('./config/passport-local-strategy');
@@ -29,6 +31,10 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+
+
+// mongo store is used to store the session cookie in the db
+
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -37,12 +43,20 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: MongoStore.create({   // for v 4 and above
+        mongoUrl: 'mongodb://localhost/codeial_development' 
+    })
+    
+
+
 }));
 
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
 
 // use express router
 app.use('/', require('./routes'));
@@ -56,3 +70,7 @@ app.listen(port, function(err){
 
     console.log(`Server is running on port: ${port}`);
 });
+
+
+
+/// npm i connect-mongo library to store the cookies in db
